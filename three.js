a21b9281fe27faef84058953fd823c58b03bf46c
rucochanman <rucochanman@users.noreply.github.com>
2020-08-12 17:34:23 +0900

    Update three.js
    Update three.js

--- three.js
+++ three.js
@@ -1,3 +1,33 @@
 window.addEventListener("load", init);
 
-function init() {}
+function init() {
+
+  //////////////////////////////////////////////////////////////////////////////////
+  //		Init
+  //////////////////////////////////////////////////////////////////////////////////
+
+  ////////////////画面設定
+
+  // サイズを指定
+  const width = 800;
+  const height = 400;
+
+  // レンダラーを作成
+  let renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#myCanvas')
+      });
+
+  renderer.setClearColor(new THREE.Color('grey'));//背景色の設定
+  document.body.appendChild( renderer.domElement );
+  renderer.setSize(width, height);
+
+
+
+
+
+
+
+
+
+
+}
