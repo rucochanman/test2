03bdb636f494a0c90aef0a39ac2b4ab386cfb91a
rucochanman <rucochanman@users.noreply.github.com>
2020-08-13 17:01:41 +0900

    update

--- three.js
+++ three.js
@@ -3,23 +3,16 @@ window.addEventListener("load", init);
 function init() {
 
   //////////////////////////////////////////////////////////////////////////////////
-  //		Init
+  //		settings
   //////////////////////////////////////////////////////////////////////////////////
 
-  ////////////////画面設定
-
-  // サイズを指定
+  // 画面サイズを指定
   const width = window.innerWidth;
   const height = window.innerHeight;
-  //const width = 800;
-  //const height = 600;
 
-  // レンダラーを作成
-  let renderer = new THREE.WebGLRenderer({
-        canvas: document.querySelector('#myCanvas')
-      });
-
-  renderer.setClearColor(new THREE.Color('green'));//背景色の設定
+  // レンダラー 背景色の設定
+  let renderer = new THREE.WebGLRenderer({　canvas: document.querySelector('#myCanvas')　});
+  renderer.setClearColor(new THREE.Color('grey'));
   document.body.appendChild( renderer.domElement );
   renderer.setSize(width, height);
 
@@ -31,15 +24,21 @@ function init() {
   camera.position.set(0, 0, 10);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
+  // ライトの設置
   //let ambLight = 1;
   //const envlight = new THREE.AmbientLight(0xffffff, ambLight);
   //scene.add(envlight);
 
-  // ライト
   var directionalLight = new THREE.DirectionalLight('#ffffff', 1);
   directionalLight.position.set(10, 10, 0);
   scene.add(directionalLight);
 
+
+
+
+
+
+  //test用
   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
   var material = new THREE.MeshPhongMaterial( {color: 0xff0000} );
   var cube = new THREE.Mesh( geometry, material );
@@ -47,6 +46,93 @@ function init() {
   cube.rotation.x = 0.5;
   cube.rotation.z = 0.5;
   scene.add( cube );
+
+
+  //////////////////////////////////////////////////////////////////////////////////
+  //		defs
+  //////////////////////////////////////////////////////////////////////////////////
+
+
+  //common-def
+  const NUM_POS = 3;
+  const VER_RECT = NUM_POS * 4;
+  const PI = Math.PI;
+  const CLOSE = false;
+  const OPEN = true;
+  const LEFT = false;
+  const RIGHT = true;
+  const center2 = new THREE.Vector2();
+  const center3 = new THREE.Vector3();
+
+  //pipe var
+  let pipeRad = 0;
+  let lowerPipePos = new THREE.Vector2();
+
+  //resolusion
+  const reso = 3; //解像度の変更
+  const headNode = 8 * reso;
+  const headEdge = 6 * reso;
+  const pipeEdge = 4 * reso;
+  const pipeNode = 4 * reso;
+  const bodyNode = 4 * reso;
+  const bodyEdge = 4 * reso;
+
+  //size
+  const wScale = 100; //サイズの変更
+  const armLength = wScale / 9;
+  const armLength2 = armLength * 1.2;
+  const armThick = wScale / 17;
+  const footLength = wScale / 11;
+  const footLength2 = footLength * 1.5;
+  const footThick = wScale / 16;
+  const headSize = wScale / 1.25;
+  const bodyLength = wScale / 4.7;
+  const bodyWidth = wScale / 4.1;
+  const line_width = wScale / 50;
+  let upperArmThick = new Array(pipeNode);
+  let lowerArmThick = new Array(pipeNode);
+  let lowerArmWidth = new Array(pipeNode);
+  let upperFootThick = new Array(pipeNode);
+  let lowerFootThick = new Array(pipeNode);
+  let ankleThick = new Array(pipeNode);
+  let bodyWidths = new Array(bodyNode);
+  let bodyThicks = new Array(bodyNode);
+
+  /*
+  //material
+  const loader = new THREE.TextureLoader();
+  const vert = document.getElementById('vs').textContent;
+  const mono_frag = document.getElementById('fs_mono').textContent;
+  const uv_frag = document.getElementById('fs_uv').textContent;
+  const bi_frag = document.getElementById('fs_bi').textContent;
+
+  let uniform = THREE.UniformsUtils.merge([
+    THREE.UniformsLib['lights'],{
+    'uTexture': { value: null },
+    'uTone': { value: null },
+    'uColor1': { value: null },
+    'uColor2': { value: null }
+    }
+  ] );
+
+  let material = new THREE.ShaderMaterial({
+    side:THREE.DoubleSide,
+    uniforms: uniform,
+    vertexShader: vert,
+    fragmentShader: null,
+    lights: true
+  });
+
+  let monoMat = material.clone();
+  monoMat.fragmentShader = mono_frag;
+  let uvMat = material.clone();
+  uvMat.fragmentShader = bi_frag;
+  */
+
+
+  //////////////////////////////////////////////////////////////////////////////////
+  //		render the whole thing on the page
+  //////////////////////////////////////////////////////////////////////////////////
 
   function sceneUpdate(){
     requestAnimationFrame( sceneUpdate );
