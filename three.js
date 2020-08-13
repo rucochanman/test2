9ca49ed4fdfbf7b5ccd1ddefb3445d3d40c38d5c
rucochanman <rucochanman@users.noreply.github.com>
2020-08-13 14:06:49 +0900

    Update three.js

--- three.js
+++ three.js
@@ -11,8 +11,8 @@ function init() {
   // サイズを指定
   //const width = window.innerWidth;
   //const height = window.innerHeight;
-  const width = 800;
-  const height = 400;
+  const width = 400;
+  const height = 600;
 
   // レンダラーを作成
   let renderer = new THREE.WebGLRenderer({
