198396e366643f06a54a88880c4b707a4bb35fa7
rucochanman <rucochanman@users.noreply.github.com>
2020-08-13 16:40:56 +0900

    Update three.js

--- three.js
+++ three.js
@@ -19,7 +19,7 @@ function init() {
         canvas: document.querySelector('#myCanvas')
       });
 
-  renderer.setClearColor(new THREE.Color('grey'));//背景色の設定
+  renderer.setClearColor(new THREE.Color('green'));//背景色の設定
   document.body.appendChild( renderer.domElement );
   renderer.setSize(width, height);
 
@@ -27,18 +27,25 @@ function init() {
   let scene = new THREE.Scene();
 
   // カメラを作成
-  let camera = new THREE.PerspectiveCamera(45, width / height, 1, 10);
-  //const camera = new THREE.OrthographicCamera(-480, +480, 270, -270, 1, 1000);
-  camera.position.set(0, 0, 0);
+  let camera = new THREE.PerspectiveCamera(15, width / height, 1, 1000);
+  camera.position.set(0, 0, 10);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-  let ambLight = 1;
-  const envlight = new THREE.AmbientLight(0xffffff, ambLight);
-  scene.add(envlight);
+  //let ambLight = 1;
+  //const envlight = new THREE.AmbientLight(0xffffff, ambLight);
+  //scene.add(envlight);
+
+  // ライト
+  var directionalLight = new THREE.DirectionalLight('#ffffff', 1);
+  directionalLight.position.set(10, 10, 0);
+  scene.add(directionalLight);
 
   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+  var material = new THREE.MeshPhongMaterial( {color: 0xff0000} );
   var cube = new THREE.Mesh( geometry, material );
+  cube.position.set(0, 0, 0);
+  cube.rotation.x = 0.5;
+  cube.rotation.z = 0.5;
   scene.add( cube );
 
   function sceneUpdate(){
